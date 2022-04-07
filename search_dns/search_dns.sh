6898d381e96f2f0dd660471db37c00c3bf6fdb00
yuan <zu00423828@gmail.com>
2022-04-07 13:55:06 +0800

    modify search server add new server

--- search_dns/search_dns.sh
+++ search_dns/search_dns.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 LIST=(
+	"hdv.tw"
+	"www.hdv.tw"
+	"sjcorp.com.tw"
+	"www.sjcorp.com.tw"
+	"gdpr.sjcorp.com.tw"
+	"machine-video.com"
+	"www.machine-video.com"
+	"enmvc.machine-video.com"
+	"enquiry.com.tw"
+	"form.enquiry.com.tw"
+	"jerryson.cn"
+	"www.jerryson.cn"
 	"$@"
 	)
 echo "start search"
@@ -10,11 +22,20 @@ for item in "${LIST[@]}"; do
  	nslookup $item|grep Address|tail -n 1|cut -f2 -d:
 done
 echo
-echo "--check Domain Name  in zoo server--"
+echo "--check Domain Name  in zoo us server--"
 for item in "${LIST[@]}"; do
-	if nslookup $item|grep -q "203.98.64.145";then
+	if nslookup $item|grep -q "203.98.64.144\|203.98.64.145";then
 		echo $item
  	fi
 done
 echo
+
+echo "--check Domain Name  in zoo tw server--"
+for item in "${LIST[@]}"; do
+	if nslookup $item|grep -q "61.221.12.84";then
+		echo $item
+ 	fi
+done
+
+
 echo "search finmish"
