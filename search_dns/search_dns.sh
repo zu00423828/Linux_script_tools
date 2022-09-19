6fe66f2c1296652ce4e894f52485dcfd0d029aac
yuan <zu00423828@gmail.com>
2022-09-19 16:44:59 +0800

    modify search dns script

--- search_dns/search_dns.sh
+++ search_dns/search_dns.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
+server_list=()
 LIST=(
-	"hdv.tw"
-	"www.hdv.tw"
-	"sjcorp.com.tw"
-	"www.sjcorp.com.tw"
-	"gdpr.sjcorp.com.tw"
-	"machine-video.com"
-	"www.machine-video.com"
-	"enmvc.machine-video.com"
-	"enquiry.com.tw"
-	"form.enquiry.com.tw"
-	"jerryson.cn"
-	"www.jerryson.cn"
 	"$@"
 	)
-echo "start search"
-echo
-echo "--echo Domain Name's IP Address--"
-for item in "${LIST[@]}"; do
-	echo ------- $item -------
- 	nslookup $item|grep Address|tail -n 1|cut -f2 -d:
-done
-echo
-echo "--check Domain Name  in zoo us server--"
-for item in "${LIST[@]}"; do
-	if nslookup $item|grep -q "203.98.64.144\|203.98.64.145";then
-		echo $item
- 	fi
-done
-echo
+echo "-- server search initialize --"
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "  |-->" $line
+    server_list+=($line)
+done < "server.list"
 
-echo "--check Domain Name  in zoo tw server--"
+echo -e "\n-- start search  --\n"
+echo "  -web domain : server ip-   "
 for item in "${LIST[@]}"; do
-	if nslookup $item|grep -q "61.221.12.84";then
-		echo $item
- 	fi
+ 	ip=`nslookup $item|grep Address|tail -n 1|cut -f2 -d:`
+    echo "    |-->" $item : $ip
 done
 
-
-echo "search finmish"
+len=${#server_list[@]}
+for ((i=0;i<$len;i+=2));do
+    echo -e "\n  -check Domain Name in ${server_list[$i]} server-   "
+    for item in "${LIST[@]}"; do
+        if nslookup $item|grep -q ${server_list[$i+1]};then
+            echo "    |-->" $item
+        fi
+    done
+done
+echo -e "\n-- search finish -- "
