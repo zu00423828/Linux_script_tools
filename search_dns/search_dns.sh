1a8cf66f1f79b6d274741f853dbaab4e37fea428
yuan <zu00423828@gmail.com>
2023-01-09 11:09:13 +0800

    modify search dns format content

--- search_dns/search_dns.sh
+++ search_dns/search_dns.sh
@@ -3,24 +3,24 @@ server_list=()
 LIST=(
 	"$@"
 	)
-echo "-- server search initialize --"
+echo -e "\n-- server search initialize --"
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "  |-->" $line
     server_list+=($line)
 done < "server.list"
 
 echo -e "\n-- start search  --\n"
-echo "  -web domain : server ip-   "
+echo "  - web domain : server ip -   "
 for item in ${LIST[@]}; do
  	ip=`nslookup $item|grep Address|tail -n 1|cut -f2 -d:`
     echo "    |-->" $item : $ip
 done
 
 len=${#server_list[@]}
-for ((i=0;i<$len;i+=2));do
-    echo -e "\n  -check Domain Name in ${server_list[$i]} server-   "
+for ((i=0;i<$len;i+=2)); do
+    echo -e "\n  - check Domain Name in ${server_list[$i]} server -   "
     for item in "${LIST[@]}"; do
-        if nslookup $item|grep -q ${server_list[$i+1]};then
+        if nslookup $item|grep -q ${server_list[$i+1]}; then
             echo "    |-->" $item
         fi
     done
